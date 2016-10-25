893281e0fa80883052975855e74b0974b1f00137
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2016-10-25 12:56:55 +0200

    Make affine check optional

--- pymaskplot/maskplotter.py
+++ pymaskplot/maskplotter.py
@@ -5,22 +5,46 @@ from scipy import ndimage
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+def plot_mask_straight_edges(mask, **kwargs):
+    
+    l = None
+    
+    color = kwargs.get('c', 'black')
+    
+    xs, ys = np.where(np.diff(mask, axis=1))    
+    
+    for x, y in zip(xs, ys):
+        plt.plot([x-.5, x+.5], [y+.5, y+.5], **kwargs)
+        
+
+    xs, ys = np.where(np.diff(mask, axis=0) != 0)    
+    
+    for x, y in zip(xs, ys):
+        l = plt.plot([x+.5, x+.5], [y-.5, y+.5], **kwargs)        
+        
+    if l:        
+        return l[0]
+
 class MaskPlotter(object):
     
     orientations = ['sagittal', 'coronal', 'axial']
     crop_margin = 15
     
-    def __init__(self, data, masks, mask_labels=None, palette=None, threshold=0.0):
+    def __init__(self, data, masks, mask_labels=None, palette=None, threshold=0.0, check_affines=True):
         
         self.masks = [nb.load(mask) for mask in masks]
         self.masks_ = [mask.get_data() > threshold for mask in self.masks]
         
-        assert(np.all([(mask.affine == self.masks[0].affine).all() for mask in self.masks]))
         
         self.data = nb.load(data)
         self.data_ = self.data.get_data()
 
-        assert((self.data.affine == self.masks[0].affine).all())
+        if check_affines:
+            assert(np.all([(mask.affine == self.masks[0].affine).all() for mask in self.masks]))
+            assert((self.data.affine == self.masks[0].affine).all())
+        else:
+            print '***WARNING*** affines are not checked'
         
         if not mask_labels:
             self.mask_labels = ['mask %d' % i for i in range(0+1, len(self.masks)+1)]
@@ -40,27 +64,46 @@ class MaskPlotter(object):
         self._zlim = None
         
         
-    def plot_masks(self, orientation='coronal', s=None, center_on_mask=None, crop=True, imshow_kwargs={}, **kwargs):
+    def plot_masks(self, orientation='coronal', s=None, center_on_mask=None, crop=True, imshow_kwargs={}, plotting_procedure='contour', **kwargs):
+       
+
+ 
         
-        
-        if orientation not in self.orientations:
+        if orientation not in self.orientations + ['all']:
             raise NotImplementedError('{orientation} orientation not implemented'.format(orientation))        
-        
-        if not center_on_mask:
+
+        if len(self.masks) > 0 and not center_on_mask:
             center_on_mask = self.mask_labels[0]
+
+        if orientation == 'all':
+            plt.subplot(131)
+            self.plot_masks('coronal', center_on_mask=center_on_mask, crop=crop, imshow_kwargs=imshow_kwargs, **kwargs)
+            plt.subplot(132)
+            self.plot_masks('sagittal', center_on_mask=center_on_mask, crop=crop, imshow_kwargs=imshow_kwargs, **kwargs)
+            plt.subplot(133)
+            l = self.plot_masks('axial', center_on_mask=center_on_mask, crop=crop, imshow_kwargs=imshow_kwargs, **kwargs)
+            return l
+
         
         if not s:
-            s = ndimage.center_of_mass(self._get_mask(center_on_mask))[self.orientations.index(orientation)]
+            if len(self.masks) > 0:
+                s = ndimage.center_of_mass(self._get_mask(center_on_mask))[self.orientations.index(orientation)]
+            else:
+                raise Exception('No mask to get center-of-mass, please provide slice number')
             
             
         idx = [slice(None), slice(None), slice(None)]
         idx[self.orientations.index(orientation)] = s
         
-    
-        plt.imshow(self.data_[idx].T, origin='lower', cmap=plt.cm.gray, interpolation='nearest', **imshow_kwargs)
-
-        for i, mask in enumerate(self.mask_labels):
-            l = plt.contour(self._get_mask(mask)[idx].T, levels=[0, 1], colors=[self.palette[i]], **kwargs) 
+        cmap = imshow_kwargs.pop('cmap', plt.cm.gray) 
+        plt.imshow(self.data_[idx].T, origin='lower', interpolation='nearest', cmap=cmap, **imshow_kwargs)
+        
+        if plotting_procedure == 'contour':
+            for i, mask in enumerate(self.mask_labels):
+                l = plt.contour(self._get_mask(mask)[idx].T, levels=[0, 1], colors=[self.palette[i]], **kwargs) 
+        elif plotting_procedure == 'straight_edges':
+            for i, mask in enumerate(self.mask_labels):
+                l = plot_mask_straight_edges(self._get_mask(mask)[idx], c=self.palette[i], **kwargs) 
             
         
 
@@ -76,6 +119,8 @@ class MaskPlotter(object):
             if orientation == 'axial':
                 plt.xlim(self.xlim[0] - self.crop_margin, self.xlim[1] + self.crop_margin)
                 plt.ylim(self.ylim[0] - self.crop_margin, self.ylim[1] + self.crop_margin)
+
+            
 
         plt.title('Slice %d (%s)' % (s, orientation))
         return l
